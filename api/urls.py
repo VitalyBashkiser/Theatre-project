e5751a78b8f13fd9c7444f9e95f6b6bedabce97d
Vitalii <vitalikbashkiser@gmail.com>
2024-06-12 10:27:49 +0300

    update file api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,24 +1,42 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views import (
-    ActorViewSet,
     GenreViewSet,
     PlayViewSet,
     TheatreHallViewSet,
-    PerformanceViewSet,
     ReservationViewSet,
     TicketViewSet,
+    ActorListCreateView,
+    ActorRetrieveUpdateDestroyView,
+    PerformanceListCreateView,
+    PerformanceRetrieveUpdateDestroyView,
 )
 
 app_name = "api"
 
 router = DefaultRouter()
-router.register(r"actors", ActorViewSet)
 router.register(r"genres", GenreViewSet)
 router.register(r"plays", PlayViewSet)
 router.register(r"theatre-halls", TheatreHallViewSet)
-router.register(r"performances", PerformanceViewSet)
 router.register(r"reservations", ReservationViewSet)
 router.register(r"tickets", TicketViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("actors/", ActorListCreateView.as_view(), name="actor-list-create"),
+    path(
+        "actors/<int:pk>/",
+        ActorRetrieveUpdateDestroyView.as_view(),
+        name="actor-retrieve-update-destroy",
+    ),
+    path(
+        "performances/",
+        PerformanceListCreateView.as_view(),
+        name="performance-list-create",
+    ),
+    path(
+        "performances/<int:pk>/",
+        PerformanceRetrieveUpdateDestroyView.as_view(),
+        name="performance-retrieve-update-destroy",
+    ),
+]
