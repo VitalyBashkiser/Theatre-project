46bb0013774a4b27ddf981101f26244e8c7597ff
Vitalii <vitalikbashkiser@gmail.com>
2024-06-12 10:28:19 +0300

    update file theatre/settings.py

--- theatre/settings.py
+++ theatre/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # user apps
     "api",
-    'users',
+    "users",
     # 3rd apps
     "drf_spectacular",
     "rest_framework",
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
     ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SPECTACULAR_SETTINGS = {
